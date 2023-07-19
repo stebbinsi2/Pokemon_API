883f613480b07895245ccf54cd1c0ea9df5f1f7a
root <root@Ians-Domain>
2023-07-19 13:28:27 -0400

    first commit for Pokelixir

---
+++ pokelixir/lib/pokelixir.ex
@@ -0,0 +1,18 @@
+defmodule Pokelixir do
+  @moduledoc """
+  Documentation for `Pokelixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pokelixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
